a59bf6a4ef47c2845c9df8e259247127663df92d
Ariel_Vargas <cav1074@gmail.com>
2024-08-08 22:04:39 -0300

    Arreglé la duplicación de la Navbar

--- rece-tario/src/App.jsx
+++ rece-tario/src/App.jsx
@@ -32,35 +32,35 @@ const App = () => {
   return (
     <Router>
       <AuthProvider>
-        <Layout>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<ProtectedRoute element={<Home />} />} />
-            <Route path="/add-recipe" element={<ProtectedRoute element={<AddRecipe />} />} />
-            <Route path="/edit-recipe/:id" element={<ProtectedRoute element={<EditRecipe />} />} />
-            <Route path="/recipe/:id" element={<ProtectedRoute element={<RecipeDetail />} />} />
-            <Route path="/categories" element={<ProtectedRoute element={<CategoryList />} />} />
-            <Route path="/add-category" element={<ProtectedRoute element={<AddCategory />} />} />
-            <Route path="/edit-category/:id" element={<ProtectedRoute element={<EditCategory />} />} />
-            <Route path="/comments" element={<ProtectedRoute element={<CommentList />} />} />
-            <Route path="/add-comment" element={<ProtectedRoute element={<AddComment />} />} />
-            <Route path="/edit-comment/:id" element={<ProtectedRoute element={<EditComment />} />} />
-            <Route path="/ingredients" element={<ProtectedRoute element={<IngredientList />} />} />
-            <Route path="/add-ingredients" element={<ProtectedRoute element={<AddIngredients />} />} />
-            <Route path="/edit-ingredients/:id" element={<ProtectedRoute element={<EditIngredients />} />} />
-            <Route path="/recipes" element={<ProtectedRoute element={<RecipeList />} />} />
-            <Route path="/ratings" element={<ProtectedRoute element={<RatingsList />} />} />
-            <Route path="/ratings/add" element={<ProtectedRoute element={<AddRating />} />} />
-            <Route path="/ratings/edit/:id" element={<ProtectedRoute element={<EditRating />} />} />
-            <Route path="/ratings/delete/:id" element={<ProtectedRoute element={<DeleteRating />} />} />
-            <Route path="/ratings/:id" element={<ProtectedRoute element={<RatingDetail />} />} />
-            <Route path="/recipe/:id/add-rating" element={<ProtectedRoute element={<AddRecipeRating />} />} />
-            <Route path="/recipes/for-rating" element={<ProtectedRoute element={<RecipeListForRating />} />} />
-            
-            <Route path="/locations" element={<ProtectedRoute element={<LocationList />} />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
+        
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ProtectedRoute element={<Home />} />} />
+          <Route path="/add-recipe" element={<ProtectedRoute element={<AddRecipe />} />} />
+          <Route path="/edit-recipe/:id" element={<ProtectedRoute element={<EditRecipe />} />} />
+          <Route path="/recipe/:id" element={<ProtectedRoute element={<RecipeDetail />} />} />
+          <Route path="/categories" element={<ProtectedRoute element={<CategoryList />} />} />
+          <Route path="/add-category" element={<ProtectedRoute element={<AddCategory />} />} />
+          <Route path="/edit-category/:id" element={<ProtectedRoute element={<EditCategory />} />} />
+          <Route path="/comments" element={<ProtectedRoute element={<CommentList />} />} />
+          <Route path="/add-comment" element={<ProtectedRoute element={<AddComment />} />} />
+          <Route path="/edit-comment/:id" element={<ProtectedRoute element={<EditComment />} />} />
+          <Route path="/ingredients" element={<ProtectedRoute element={<IngredientList />} />} />
+          <Route path="/add-ingredients" element={<ProtectedRoute element={<AddIngredients />} />} />
+          <Route path="/edit-ingredients/:id" element={<ProtectedRoute element={<EditIngredients />} />} />
+          <Route path="/recipes" element={<ProtectedRoute element={<RecipeList />} />} />
+          <Route path="/ratings" element={<ProtectedRoute element={<RatingsList />} />} />
+          <Route path="/ratings/add" element={<ProtectedRoute element={<AddRating />} />} />
+          <Route path="/ratings/edit/:id" element={<ProtectedRoute element={<EditRating />} />} />
+          <Route path="/ratings/delete/:id" element={<ProtectedRoute element={<DeleteRating />} />} />
+          <Route path="/ratings/:id" element={<ProtectedRoute element={<RatingDetail />} />} />
+          <Route path="/recipe/:id/add-rating" element={<ProtectedRoute element={<AddRecipeRating />} />} />
+          <Route path="/recipes/for-rating" element={<ProtectedRoute element={<RecipeListForRating />} />} />
+          
+          <Route path="/locations" element={<ProtectedRoute element={<LocationList />} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        
       </AuthProvider>
     </Router>
   );
